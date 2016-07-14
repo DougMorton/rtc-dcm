a90f25c301049fb833d05d0a9b5dd785ef1ed29a
DougMorton <doug@ludewigmultimedia.com>
2016-07-14 13:41:19 -0700

    HOST tweak

--- client/webrtc.js
+++ client/webrtc.js
@@ -16,7 +16,10 @@ function pageReady() {
     localVideo = document.getElementById('localVideo');
     remoteVideo = document.getElementById('remoteVideo');
 
-    serverConnection = new WebSocket('wss://' + window.location.hostname + ':5000');
+var HOST = location.origin.replace(/^http/, 'ws')
+var serverConnection = new WebSocket(HOST);
+
+    //serverConnection = new WebSocket('wss://' + window.location.hostname + ':5000');
     serverConnection.onmessage = gotMessageFromServer;
 
     var constraints = {
